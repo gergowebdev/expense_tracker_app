827e05b6393b3b24a2675028967a582843b76fab
Gyenes Gergo <gergo.gyenes@gmail.com>
2025-01-26 18:46:11 +0100

    Adding global colors & editing navigation configuration

--- App.js
+++ App.js
@@ -5,18 +5,48 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ManageExpense from "./screens/ManageExpense";
 import RecentExpenses from "./screens/RecentExpenses";
 import AllExpenses from "./screens/AllExpenses";
+import { GlobalStyles } from "./constants/styles";
+import { Ionicons } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
 
 function ExpensesOverview() {
     return (
-        <BottomTabs.Navigator>
+        <BottomTabs.Navigator
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: GlobalStyles.colors.primary500,
+                },
+                headerTintColor: "white",
+                tabBarStyle: {
+                    backgroundColor: GlobalStyles.colors.primary500,
+                },
+                tabBarActiveTintColor: GlobalStyles.colors.accent500,
+            }}
+        >
             <BottomTabs.Screen
                 name="RecentExpenses"
                 component={RecentExpenses}
+                options={{
+                    title: "Recent Expenses",
+                    tabBarLabel: "Recent",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="hourglass" size={size} color={color} />
+                    ),
+                }}
             />
-            <BottomTabs.Screen name="AllExpenses" component={AllExpenses} />
+            <BottomTabs.Screen
+                name="AllExpenses"
+                component={AllExpenses}
+                options={{
+                    title: "All Expenses",
+                    tabBarLabel: "All Expenses",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="calendar" size={size} color={color} />
+                    ),
+                }}
+            />
         </BottomTabs.Navigator>
     );
 }
@@ -30,6 +60,7 @@ export default function App() {
                     <Stack.Screen
                         name="ExpensesOverview"
                         component={ExpensesOverview}
+                        options={{ headerShown: false }}
                     />
                     <Stack.Screen
                         name="ManageExpense"
