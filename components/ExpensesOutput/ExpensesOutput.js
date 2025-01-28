88759d0b9d2e1c341f6bc9c5758b5e4882340f25
Gyenes Gergo <gergo.gyenes@gmail.com>
2025-01-28 15:50:09 +0100

    Improving App layout and styling

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,4 +1,5 @@
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
@@ -38,7 +39,7 @@ const DUMMY_EXPENSES = [
 
 function ExpensesOutput({ expenses, expensesPeriod }) {
     return (
-        <View>
+        <View style={styles.container}>
             <ExpensesSummary
                 expenses={DUMMY_EXPENSES}
                 periodName={expensesPeriod}
@@ -49,3 +50,11 @@ function ExpensesOutput({ expenses, expensesPeriod }) {
 }
 
 export default ExpensesOutput;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: GlobalStyles.colors.primary700,
+    },
+});
